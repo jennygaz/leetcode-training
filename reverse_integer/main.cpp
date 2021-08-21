1c54da5e239bc4e16e77702b37b69a3e244949db
jennygaz <dev.jenny.ga@gmail.com>
2021-08-20 21:30:00 -0500

    Added comments for 'Reverse Integer'

--- reverse_integer/main.cpp
+++ reverse_integer/main.cpp
@@ -12,25 +12,34 @@ using namespace std;
 // so we just set LIMIT as (2^31 - 1) / 10
 
 int reverse( int x ){
+  // this makes the cases [-2147483647, 2147483647], way easier to handle!
   if( x == -2147483648 ) return 0;
   int LIMIT = (((1 << 30) - 1) << 1) + 1; // assuming int is 32-bits or more, this is 2^31 - 1
   int ans {}, sign {};
+  // Get the sign and drop it, since we know modulus CAN be negative
   if( x >= 0 )
     sign = 1;
   else{
     sign = -1;
     x *= -1;
   }
-  while( x ){
+  while( x ){ // while the value isn't 0, no "== 0" required
+    // Whenever the answer is greater than the limit OR
+    // the case when it is inside [2147483640, 2147483649]
+    // since LIMIT / 10 gets truncated
+    // -- We could also do 1.0 * ans / LIMIT / 10
+    // but that would give floating point issues
     if( ans > LIMIT / 10 or (ans == 214748364 and x % 10 > 8 ) return 0;
     ans *= 10;
     ans += x % 10;
     x /= 10;
   }
+  // return the original sign
   return ans * sign;
 }
 
 int main(){
+  // Some tests
   vector<int> values( 5, 0 );
   values[0] = 123;
   values[1] = -123;
